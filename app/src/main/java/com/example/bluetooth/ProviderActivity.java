4953a64162380746a1ab04507d5cffd9ac266773
spenot09 <spenot09@gmail.com>
2019-03-12 13:02:28 +0000

    Verison 2

--- app/src/main/java/com/example/bluetooth/ProviderActivity.java
+++ app/src/main/java/com/example/bluetooth/ProviderActivity.java
@@ -1,7 +1,15 @@
 package com.example.bluetooth;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,9 +23,12 @@ import pl.droidsonroids.gif.GifImageView;
 
 public class ProviderActivity extends AppCompatActivity {
 
-    private Button start_service_button, stop_service_button;
+    private Button start_service_button, stop_service_button, bind_bluetooth_button;
     private TextView sensor_txt,provider_instruction_textView;
     private GifImageView gif;
+
+    private boolean bound = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,7 @@ public class ProviderActivity extends AppCompatActivity {
 
         start_service_button = findViewById(R.id.start_service_button);
         stop_service_button = findViewById(R.id.stop_service_button);
+        bind_bluetooth_button = findViewById(R.id.bind_bluetooth_button);
 
         sensor_txt = findViewById(R.id.sensing_textView);
         provider_instruction_textView = findViewById(R.id.provider_instruction_textView);
@@ -83,6 +95,87 @@ public class ProviderActivity extends AppCompatActivity {
                 provider_instruction_textView.setVisibility(View.VISIBLE);
             }
         });
+
+        bind_bluetooth_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doBindService();
+            }
+        });
+    }
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SensorService.MSG_SENSOR:
+                    if (sensor_tpye==MSG_ACCELEROMETER) {
+                        acc_textView.setText(String.format("Accelerometer value: %.1f", msg.obj));
+                        speedometer.speedTo((float)msg.obj, 500);
+                    }
+                    if (sensor_tpye==MSG_LIGHT) {
+                        light_textView.setText("Light sensor value: " + msg.obj);
+                        fire_gauge.speedTo((float)msg.obj, 500);
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mService = new Messenger(service);
+            textStatus.setText("Attached.");
+            try {
+                Message msg = Message.obtain(null, SensorService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            }
+            catch (RemoteException e) {
+                // In this case the service has crashed before we could even do anything with it
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mService = null;
+            textStatus.setText("Disconnected.");
+            bound = false;
+        }
+    };
+
+    private void sendMessageToService(int intvaluetosend) {
+        if (bound) {
+            if (mService != null) {
+                try {
+                    //need to check this if successful because may need to alter for use
+                    Message msg = Message.obtain(null, SensorService.MSG_SENSOR, intvaluetosend, 0);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                }
+                catch (RemoteException e) {
+                }
+            }
+        }
+    }
+
+    void doBindService() {
+        bindService(new Intent(this, SensorService.class), connection, Context.BIND_AUTO_CREATE);
+        bound = true;
+        textStatus.setText("Binding.");
+    }
+
+    void doUnbindService() {
+        if (bound) {
+
+            // Detach our existing connection.
+            unbindService(connection);
+            bound = false;
+            //textStatus.setText("Unbinding.");
+        }
     }
 
     @Override
